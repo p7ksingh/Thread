8406c8dc5ff066499e1f736c1f6d9b0a5c3751ad
Pankaj Singh <p7ksingh@gmail.com>
2024-03-03 22:12:14 +0530

    Thread ex4

---
+++ Test1.java
@@ -0,0 +1,11 @@
+package com.arr.Thread;
+
+public class Test1 {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        System.out.println(Thread.currentThread());
+        System.out.println(Thread.currentThread().getClass());
+        System.out.println(Thread.currentThread().getClass().getName());
+
+    }
+}
